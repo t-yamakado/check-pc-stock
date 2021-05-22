214e8f721d55b1b55468ddfc24f3c05674836923
山門峻 <yamakado@takashi-mac.local>
2021-05-22 18:14:37 +0900

    modify: check pc stock

--- cypress/integration/index_spec.ts
+++ cypress/integration/index_spec.ts
@@ -1,7 +1,10 @@
+context("在庫のチェック", () => {
+	beforeEach(() => {
+		cy.visit(""); // {baseUrl}
+	});
 
-
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+	it("指定のPCが購入可能な状態である", () => {
+    // 要素が存在することを確認する
+    cy.get('#buyNowButton').should('exist')
+	});
+});
